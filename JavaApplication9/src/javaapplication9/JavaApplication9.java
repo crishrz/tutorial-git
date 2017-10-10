995f762f0f5121feebbca2dde4f14e6d8beac21f
crishrz <32606438+crishrz@users.noreply.github.com>
2017-10-10 18:44:41 -0400

    Revert "Initial commit"
    This reverts commit b78a5a30c6dfe121c53f75c9943961dc73b116d6.

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,22 @@
+package javaapplication9;
+public class JavaApplication9 {
+
+ private int bares;
+ private int local;
+
+ 
+ public JavaApplication9(){
+     this.bares=1;
+     this.local=1;
+ }
+ public JavaApplication9 (int bares,int local){
+     this.bares=bares;
+     this.local=local;
+     
+ }
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
